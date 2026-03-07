985f0ab31ca96b20e694f1d1708675e1c7173ba3
Chelsea Adetan <adetanchelsea@gmail.com>
2026-03-07 11:55:09 +0100

    Added a snapshot file.

---
+++ models/intermediate/driver_status_events_int.sql
@@ -0,0 +1,15 @@
+{{ config(
+materialized='incremental',
+unique_key='event_id'
+) }}
+
+select *
+
+from {{ ref('driver_status_events_stg') }}
+
+{% if is_incremental() %}
+
+where event_timestamp >
+(select max(event_timestamp) from {{ this }})
+
+{% endif %}
---
+++ snapshots/drivers_snapshot.sql
@@ -0,0 +1,14 @@
+{% snapshot drivers_snapshot %}
+
+{{
+config(
+target_schema='snapshots',
+unique_key='driver_id',
+strategy='timestamp',
+updated_at='updated_at'
+)
+}}
+
+select * from {{ ref('drivers_stg') }}
+
+{% endsnapshot %}
